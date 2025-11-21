a930266fe551b08271cff6d7921060dc89111c59
Ganjali717 <ganjali.imanov@gmail.com>
2025-11-21 20:52:23 +0100

    feat adding unit test for order creation and order status transitions, all tests passed

---
+++ src/Order.Service.Tests/CreateOrderRequestValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using Order.Model.Requests;
+using OrderService.WebAPI.Validation;
+
+namespace Order.Service.Tests
+{
+    public class CreateOrderRequestValidatorTests
+    {
+        private CreateOrderRequestValidator _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new CreateOrderRequestValidator();
+        }
+
+        [Test]
+        public void Empty_items_should_have_error()
+        {
+            var model = new CreateOrderRequest
+            {
+                ResellerId = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+                Items = new List<CreateOrderItemRequest>()
+            };
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Items);
+        }
+
+        [Test]
+        public void Non_positive_quantity_has_error()
+        {
+            var model = new CreateOrderRequest
+            {
+                ResellerId = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+                Items = new List<CreateOrderItemRequest>
+                {
+                    new()
+                    {
+                        ProductId = Guid.NewGuid(),
+                        Quantity = 0
+                    }
+                }
+            };
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor("Items[0].Quantity");
+        }
+
+        [Test]
+        public void Valid_request_has_no_errors()
+        {
+            var model = new CreateOrderRequest
+            {
+                ResellerId = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+                Items = new List<CreateOrderItemRequest>
+                {
+                    new()
+                    {
+                        ProductId = Guid.NewGuid(),
+                        Quantity = 2
+                    }
+                }
+            };
+
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ src/Order.Service.Tests/OrderStatusTransitionsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Order.Service.Exceptions;
+using Order.Service.Status;
+
+namespace Order.Service.Tests
+{
+    public class OrderStatusTransitionsTests
+    {
+        [Test]
+        public void Valid_transition_does_not_throw()
+        {
+            Assert.DoesNotThrow(() =>
+                OrderStatusTransitions.Validate("Created", "In Progress"));
+        }
+
+        [Test]
+        public void Invalid_transition_throws()
+        {
+            var ex = Assert.Throws<InvalidOrderStatusTransitionException>(() =>
+                OrderStatusTransitions.Validate("Completed", "In Progress"));
+
+            StringAssert.Contains("Cannot change status", ex.Message);
+        }
+
+        [Test]
+        public void Unknown_status_throws()
+        {
+            Assert.Throws<InvalidOrderStatusTransitionException>(() =>
+                OrderStatusTransitions.Validate("Something", "In Progress"));
+        }
+    }
+}
